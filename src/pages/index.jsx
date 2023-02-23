3d98fffd0a9c4ea3d97879d80a91868252871d3a
Lucas Adams <lucasgreeneadams@gmail.com>
2023-02-22 19:20:34 -0700

    update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -17,7 +17,7 @@ export default function Home() {
         <title>Idle Brewery</title>
         <meta
           name="description"
-          content="Idle Brewery is a game where you can build your own brewery and become the best brewer in the world."
+          content="Idle Brewery is a game where you can build your own brewery and become the best brewer in the world!"
         />
         <link rel="icon" href="/beer.png" />
       </Head>
